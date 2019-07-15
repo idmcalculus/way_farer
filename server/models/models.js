ccc01e08bb864e1a7ce8cec15d34c3c480ca1543
idmcalculus <35293275+idmcalculus@users.noreply.github.com>
2019-07-15 19:49:33 +0100

    includes bookingTable to user_db

--- server/models/models.js
+++ server/models/models.js
@@ -17,8 +17,20 @@ const models = () => {
         fare NUMERIC(15,2) NOT NULL,
         status VARCHAR NOT NULL DEFAULT 'active'
     );`;
+    const bookingsTable = `CREATE TABLE IF NOT EXISTS bookings(
+        booking_id INTEGER NOT NULL,
+        id SERIAL PRIMARY KEY,        
+        bus_id INTEGER NOT NULL,
+        trip_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        seat_number INTEGER NOT NULL,
+        first_name VARCHAR NOT NULL,
+        last_name VARCHAR NOT NULL,
+        email VARCHAR NOT NULL UNIQUE,
+        status VARCHAR NOT NULL DEFAULT 'active'
+    );`;
     pool.query(`${usersTable}
-                ${tripsTable}`)
+                ${tripsTable}
+                ${bookingsTable}`)
     .then()
     .catch();
 };
